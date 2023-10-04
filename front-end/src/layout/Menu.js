89dc86522a866cd9f61cb9412793cf61f30e3ca7
mammolastan <mammolastan@hotmail.com>
2023-10-03 22:28:58 -0400

    Cube Spinning Splash

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -14,16 +14,15 @@ function Menu() {
   return (
     <div className="container">
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-        <h1>
-          <Link
-            to="/#"
-            className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
-          >
+        <Link
+          to="/#"
+          className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+        >
+          <h1>
             <img src={sprinterLogo} />
             SkillSprint
-          </Link>
-        </h1>
-
+          </h1>
+        </Link>
         <div className="col-md-3 text-end">
           <button
             type="button"
