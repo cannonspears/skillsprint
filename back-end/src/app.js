880f0e87a5c8adb4cf2c961a76218ae56b264808
jeffjeffjeffh <jeffjeffjeffh@yahoo.com>
2023-10-04 10:01:42 -0700

    Videos API list, create, read

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,22 +1,23 @@
-const path = require("path");
-require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, '..', '.env') })
 
-const express = require("express");
-const cors = require("cors");
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
-const quizRouter = require("./quiz/quizRouter");
-const usersRouter = require("./users/usersRouter");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const errorHandler = require('./errors/errorHandler')
+const notFound = require('./errors/notFound')
+const quizRouter = require('./quiz/quizRouter')
+const videosRouter = require('./videos/videosRouter')
+const usersRouter = require('./users/usersRouter')
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/quizzes", quizRouter);
+app.use('/quizzes', quizRouter)
+app.use('/videos', videosRouter)
+app.use('/users', usersRouter)
 
-app.use("/users", usersRouter);
+app.use(notFound)
+app.use(errorHandler)
 
-app.use(notFound);
-app.use(errorHandler);
-
-module.exports = app;
+module.exports = app
