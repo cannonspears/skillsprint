41f15302a18cfbc31b6d4d8cb91d11365af28f4f
jeffjeffjeffh <jeffjeffjeffh@yahoo.com>
2023-10-05 10:56:04 -0700

    Fixed history retrieval, started history frontend api

--- back-end/src/history/history.service.js
+++ back-end/src/history/history.service.js
@@ -1,13 +1,13 @@
 const knex = require('../db/connection')
 
-function create (history) {
+function create(history) {
     return knex('history')
         .insert(history)
         .returning('*')
         .finally(() => knex.destroy())
 }
 
-function read (historyId) {
+function read(historyId) {
     return knex('history')
         .join('users', 'history.user_id', 'users.user_id')
         .join('videos', 'history.video_id', 'videos.video_id')
@@ -17,7 +17,7 @@ function read (historyId) {
         .finally(() => knex.destroy())
 }
 
-function update (updatedHistory) {
+function update(updatedHistory) {
     return knex('history')
         .where({ history_id: updatedHistory.history_id })
         .update(updatedHistory, '*')
@@ -25,19 +25,24 @@ function update (updatedHistory) {
         .finally(() => knex.destroy())
 }
 
-function remove (historyId) {
+function remove(historyId) {
     return knex('history')
         .where({ history_id: historyId })
         .del()
         .finally(() => knex.destroy())
 }
 
-function list (user_id) {
-    return knex('history')
-        .join('users', 'history.user_id', 'users.user_id')
-        .join('videos', 'history.video_id', 'videos.video_id')
-        .select('*')
-        .where({ user_id: user_id })
+function list(user_id) {
+    return knex('history as h')
+        .join('users as u', 'h.user_id', 'u.user_id')
+        .join('videos as v', 'h.video_id', 'v.video_id')
+        .select(
+            'v.video_id',
+            'v.video_title',
+            'v.video_topic',
+            'h.video_completed'
+        )
+        .where('u.user_id', user_id)
         .finally(() => knex.destroy())
 }
 
@@ -46,5 +51,5 @@ module.exports = {
     read,
     update,
     remove,
-    list
+    list,
 }
