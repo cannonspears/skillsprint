2dac5252993febb932ce8e94a5c6f8a6584d1c88
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-10-08 17:50:37 -0700

    In Progress is in progress

--- front-end/src/layout/MySkills/Progress.js
+++ front-end/src/layout/MySkills/Progress.js
@@ -6,13 +6,31 @@ export default function Progress({ history }) {
         return <p>Loading Progress...</p>
     }
 
+    const skillMap = new Map()
+
+    history.forEach((e) => {
+        if (skillMap.has(e.skill_id)) {
+            if (e.video_completed) {
+                skillMap.set(e.skill_id, skillMap.get(e.skill_id) + 1)
+            }
+        } else {
+            skillMap.set(e.skill_id, 0)
+        }
+    })
+
+    const skillArray = []
+
+    for (let [key, val] of skillMap.entries()) {
+        skillArray.push([key, val])
+    }
+
     return (
         <div className="progress">
             <ul className="items">
-                {history.map((item) => {
+                {skillArray.map(([key, val]) => {
                     return (
-                        <li key={`historycard-${item.history_id}`}>
-                            <HistoryCard item={item} />
+                        <li key={`historycard-${key}`}>
+                            <HistoryCard skill_id={key} completed={val} />
                         </li>
                     )
                 })}
