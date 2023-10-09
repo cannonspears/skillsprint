443f1f2799dbce1130aa6f3db1eb0ecb9a37451e
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-10-09 00:29:53 -0700

    Quiz fully implemented

--- front-end/src/Quiz/QuizForm.js
+++ front-end/src/Quiz/QuizForm.js
@@ -1,10 +1,22 @@
 import { useState } from 'react'
 import './QuizForm.css'
 
+function allAnswered(answers) {
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === null) {
+            return false
+        }
+    }
+    return true
+}
+
 export default function QuizForm({ quiz }) {
     const [selectedAnswers, setSelectedAnswers] = useState(
         new Array(4).fill(null)
     )
+
+    const [submitted, setSubmitted] = useState(false)
+    const [showAnswers, setShowAnswers] = useState(false)
 
     if (!quiz) {
         return <p>Loading Quiz...</p>
@@ -19,11 +31,18 @@ export default function QuizForm({ quiz }) {
     const handleSubmit = (e) => {
         e.preventDefault()
         console.log(selectedAnswers)
+        if (allAnswered(selectedAnswers)) {
+            setShowAnswers(true)
+        } else {
+            alert('Please fill out all questions before submitting!')
+        }
     }
 
     return (
         <>
-            <p>Here's your quiz! Good luck....</p>
+            <p style={{ marginLeft: '2rem' }}>
+                Here's your quiz! Good luck....
+            </p>
             <form onSubmit={handleSubmit} className="quizAnswers">
                 <ul className="quizList">
                     {quiz.map((item, i) => {
@@ -57,11 +76,29 @@ export default function QuizForm({ quiz }) {
                                         </div>
                                     ))}
                                 </div>
+                                {showAnswers ? (
+                                    <div className="answer">
+                                        <p>
+                                            {selectedAnswers[i] ===
+                                            item.answer ? (
+                                                <p className="right">
+                                                    Right answer!
+                                                </p>
+                                            ) : (
+                                                <p className="wrong">
+                                                    Wrong answer!
+                                                </p>
+                                            )}
+                                        </p>
+                                    </div>
+                                ) : null}
                             </li>
                         )
                     })}
                 </ul>
-                <button type="submit">Submit Quiz</button>
+                {showAnswers ? null : (
+                    <button type="submit">Submit Quiz</button>
+                )}
             </form>
         </>
     )
