0a452014455bceb49c447c71c99d4d44f673f979
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-10-08 17:58:29 -0700

    Completed completed?

--- front-end/src/layout/MySkills/Completed.js
+++ front-end/src/layout/MySkills/Completed.js
@@ -1,5 +1,40 @@
-import "./Completed.css"
+import './Completed.css'
+import HistoryCard from './HistoryCard'
 
-export default function Completed() {
-    return <p>completed</p>
+export default function Completed({ history }) {
+    if (!history) {
+        return <p>Loading Progress...</p>
+    }
+
+    const skillMap = new Map()
+
+    history.forEach((e) => {
+        if (skillMap.has(e.skill_id)) {
+            if (e.video_completed) {
+                skillMap.set(e.skill_id, skillMap.get(e.skill_id) + 1)
+            }
+        } else {
+            skillMap.set(e.skill_id, 0)
+        }
+    })
+
+    const skillArray = []
+
+    for (let [key, val] of skillMap.entries()) {
+        skillArray.push([key, val])
+    }
+
+    return (
+        <div className="completed">
+            <ul className="items">
+                {skillArray.map(([key, val]) => {
+                    return (
+                        <li key={`historycard-${key}`}>
+                            <HistoryCard skill_id={key} completed={val} />
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+    )
 }
