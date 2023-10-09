8e752cd41bb76ca9e362f9473304313a9e38d0f6
jeffjeffjeffh <121068607+jeffjeffjeffh@users.noreply.github.com>
2023-10-08 19:43:51 -0700

    oops

--- front-end/src/layout/Explore/Explore.js
+++ front-end/src/layout/Explore/Explore.js
@@ -2,35 +2,33 @@ import { useState, useEffect } from 'react'
 import { fetchAllVideos } from '../../utils/videosApi'
 import SkillCard from './SkillCard'
 import './Explore.css'
-import Quiz from '../../Quiz/Quiz'
 export default function Explore() {
-    return <Quiz />
-    // const [skillVids, setSkillVids] = useState(null)
+    const [skillVids, setSkillVids] = useState(null)
 
-    // useEffect(() => {
-    //     async function getSkills() {
-    //         const apiData = await fetchAllVideos()
-    //         setSkillVids(apiData)
-    //     }
+    useEffect(() => {
+        async function getSkills() {
+            const apiData = await fetchAllVideos()
+            setSkillVids(apiData)
+        }
 
-    //     getSkills()
-    // }, [])
+        getSkills()
+    }, [])
 
-    // return (
-    //     <>
-    //         {!skillVids ? (
-    //             <p>Loading Skills...</p>
-    //         ) : (
-    //             <ul className="explore">
-    //                 {skillVids.map((videos) => {
-    //                     return (
-    //                         <li key={`skill-${videos[0].skill_id}`}>
-    //                             <SkillCard videos={videos} />
-    //                         </li>
-    //                     )
-    //                 })}
-    //             </ul>
-    //         )}
-    //     </>
-    // )
+    return (
+        <>
+            {!skillVids ? (
+                <p>Loading Skills...</p>
+            ) : (
+                <ul className="explore">
+                    {skillVids.map((videos) => {
+                        return (
+                            <li key={`skill-${videos[0].skill_id}`}>
+                                <SkillCard videos={videos} />
+                            </li>
+                        )
+                    })}
+                </ul>
+            )}
+        </>
+    )
 }
